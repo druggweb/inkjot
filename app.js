37b38139200220d2a27dd886ea18db95e3b20207
Nick Ruggero <druggero7@gmail.com>
2018-03-03 23:20:38 -0500

    Finished Lecture 17

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const uncaught = require('uncaught');
+// const MakePromisesSafe = require('make-promises-safe')Not Working;
 
 const app = express();
 
-// Map global promise - get rid of warning
-mongoose.Promise = global.Promise;
-// Connect to Mongoose
-mongoose.connect('mongodb://localhost/inkjot-dev', {
-  useMongoClient: true
-})
+mongoose.connect('mongodb://localhost/inkjot-dev')
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
+
+// Uncaught Package
+uncaught.start();
+uncaught.addListener(function (error) {
+    console.log('Uncaught error or rejection: ', error.message);
+});
 
 // Load Idea Model
 require('./models/Idea');
@@ -22,6 +26,10 @@ app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // Index Route
 app.get('/', (req, res) => {
@@ -34,6 +42,41 @@ app.get('/', (req, res) => {
 // About Route
 app.get('/about', (req, res) => {
   res.render('about');
+});
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+// Process Form
+app.post('/ideas', (req, res) => {
+  let errors = [];
+
+  if(!req.body.title){
+    errors.push({text:'Please add a title'});
+  }
+  if(!req.body.details){
+    errors.push({text:'Please add some details'});
+  }
+
+  if(errors.length > 0){
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    }
+    new Idea(newUser)
+      .save()
+      .then(idea => {
+        res.redirect('/ideas');
+      })
+  }
 });
 
 const port = 5000;
--- models/Idea.js
+++ models/Idea.js
@@ -11,10 +11,6 @@ const IdeaSchema = new Schema({
     type: String,
     required: true
   },
-  user:{
-    type: String,
-    required:true
-  },
   date: {
     type: Date,
     default: Date.now
